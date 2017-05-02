518bdb3f8a634651e75168f19dd37181b33785e5
James Lennon <jameslennon321@gmail.com>
2017-05-01 21:48:59 -0400

    Added page content

---
+++ res/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+	$(".project-card").click(function() {
+    	page_url        = $(this).attr("href");
+    	window.location = page_url
+    });
+
+
+	/* Homepage animations */
+
+	var viewed = false;
+
+	$("#articles-grid").appear();
+
+	$.each($(".project-card"), function(key, value) {
+		$(value).hide();
+	});
+
+	$(document.body).on('appear', function(){
+		if (!viewed) {
+			viewed = true;
+			$.each($(".project-card"), function(key, value) {
+				$(value).delay(key * 100 + 500).fadeIn();
+			});
+		}
+	});
+
+	
+
+	/* Article animations */
+
+	$("h1").hide();
+    $("p").hide();
+
+    $("h1").transition("fade up", '500ms');
+    $("p").delay(500).transition('fade up', '500ms');
+
+    $('#home-button')
+	  .sticky({
+	    context: 'h1'
+	  })
+	;
+
+
+});
+
